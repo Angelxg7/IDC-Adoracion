024efb99b0a0e3ccaa0e14bda5ae54e0088ffa62
angelxg7 <gabrielmartincornejo@gmail.com>
2024-04-13 20:52:14 -0300

    commit1304a

---
+++ menu.js
@@ -0,0 +1,18 @@
+const subMenuBtn = document.querySelectorAll(".submenu-btn");
+for(let i=0; i<subMenuBtn.length; i++) {
+	subMenuBtn[i].addEventListener("click", function(){
+		
+			const subMenu = this.nextElementSibling;
+			const height = subMenu.scrollHeight;
+
+			if(subMenu.classList.contains("desplegar")){
+				subMenu.classList.remove("desplegar");
+				subMenu.removeAttribute("style");
+			} else {
+				subMenu.classList.add("desplegar");
+				subMenu.style.height = height + "px";
+			}
+
+		
+	});
+}
